bb58c24f041e10765e55e9543c1f863dd9714e96
kahau2005 <2351050043hau@ou.edu.vn>
2026-03-31 09:20:15 +0700

    first configs

---
+++ LearnSpaceBackend/src/main/java/com/learnspace/learnspacebackend/configs/DispatcherServletInit.java
@@ -0,0 +1,24 @@
+package com.learnspace.learnspacebackend.configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+@Configuration
+public class DispatcherServletInit extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[] {ThymeleafConfigs.class, HibernateConfigs.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] {
+            WebAppContextConfigs.class,
+        };
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
+    }
+}
---
+++ LearnSpaceBackend/src/main/java/com/learnspace/learnspacebackend/configs/WebAppContextConfigs.java
@@ -0,0 +1,36 @@
+package com.learnspace.learnspacebackend.configs;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.web.multipart.support.StandardServletMultipartResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@ComponentScan(basePackages = {"com.learnspace.learnspacebackend.controllers"})
+@EnableWebMvc
+public class WebAppContextConfigs implements WebMvcConfigurer {
+
+    @Autowired
+    private Environment env;
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/js/**").addResourceLocations("classpath:/static/js/");
+    }
+
+    @Bean
+    public StandardServletMultipartResolver multipartResolver() {
+        return new StandardServletMultipartResolver();
+    }
+}
