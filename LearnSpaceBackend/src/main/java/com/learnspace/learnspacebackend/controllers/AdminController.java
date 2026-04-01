9bc5e8ac44f7d93c232e33426d2caf0dc129c795
kahau2005 <2351050043hau@ou.edu.vn>
2026-04-01 10:47:01 +0700

    response data to view

--- LearnSpaceBackend/src/main/java/com/learnspace/learnspacebackend/controllers/AdminController.java
+++ LearnSpaceBackend/src/main/java/com/learnspace/learnspacebackend/controllers/AdminController.java
@@ -1,6 +1,8 @@
 package com.learnspace.learnspacebackend.controllers;
 
 import com.learnspace.learnspacebackend.pojo.Category;
+import com.learnspace.learnspacebackend.services.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +11,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("admin")
 public class AdminController {
+    @Autowired
+    private CategoryService categoryService;
+
     @GetMapping()
     public String admin() {
         return "admin";
@@ -36,6 +41,7 @@ public class AdminController {
 
     @GetMapping("/categories")
     public String adminCategory(Model model) {
+        model.addAttribute("categories", categoryService.getCates());
         model.addAttribute("category", new Category());
         return "admin_categories";
     }
