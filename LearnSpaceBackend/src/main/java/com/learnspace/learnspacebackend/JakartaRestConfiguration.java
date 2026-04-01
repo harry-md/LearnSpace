55d08d1fef17dd06bc3d76c781866595b9a079ab
harry-md <hai.maoduc@gmail.com>
2026-04-01 12:07:38 +0700

    reformat code

--- LearnSpaceBackend/src/main/java/com/learnspace/learnspacebackend/JakartaRestConfiguration.java
+++ LearnSpaceBackend/src/main/java/com/learnspace/learnspacebackend/JakartaRestConfiguration.java
@@ -3,11 +3,5 @@ package com.learnspace.learnspacebackend;
 import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.core.Application;
 
-/**
- * Configures Jakarta RESTful Web Services for the application.
- * @author Juneau
- */
 @ApplicationPath("resources")
-public class JakartaRestConfiguration extends Application {
-    
-}
+public class JakartaRestConfiguration extends Application {}
