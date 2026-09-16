dfb9203031963135be80e146cda86e823a072b4d
harry-md <hai.maoduc@gmail.com>
2026-09-16 23:32:09 +0700

    update

--- LearnSpaceBackend/src/main/java/com/learnspace/learnspacebackend/configs/ApiSecurityConfigs.java
+++ LearnSpaceBackend/src/main/java/com/learnspace/learnspacebackend/configs/ApiSecurityConfigs.java
@@ -76,7 +76,7 @@ public class ApiSecurityConfigs {
     public CorsConfigurationSource apiCorsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(
-                List.of("http://localhost:5173", "https://learn-space-three.vercel.app/"));
+                List.of("http://localhost:5173", "https://learn-space-three.vercel.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         config.setExposedHeaders(List.of("Authorization"));
